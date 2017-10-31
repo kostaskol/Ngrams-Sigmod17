794a372424e4fa798514e5b185b1a64a0c7c74d3
Giannis Argyros <sdi1200009@di.uoa.gr>
2017-10-31 20:20:08 +0200

    updated from kostas

--- include/trie.hpp
+++ include/trie.hpp
@@ -7,6 +7,7 @@
 #include "mqueue.hpp"
 #include "hash_table.hpp"
 
+
 class trie {
 private:
     class trie_node {
@@ -22,7 +23,7 @@ private:
 
         trie_node(const std::string &word, bool eow, trie_node *par = nullptr);
 
-        trie_node(const trie_node &other);
+        trie_node(const trie_node &other)=delete;
 
         trie_node(trie_node &&other) noexcept;
 
@@ -30,7 +31,13 @@ private:
 
         trie_node *add_child(int index, std::string word, bool eow);
 
+        void remove_child(int index);
+
         const mstd::vector<trie_node> &get_children();
+
+        mstd::vector<trie::trie_node> *get_children_p();
+
+		trie_node *get_parent();
 
         trie_node *get_child(int index);
 
@@ -38,15 +45,19 @@ private:
 
         void push_child(trie_node *node);
 
-        bool is_end_of_word();
+        bool is_end_of_word() const;
 
-        size_t children_size();
+        size_t children_size() const;
 
-        std::string get_word();
+        std::string get_word() const;
 
         void set_end_of_word(bool v);
 
         void print(int level);
+
+        void to_string(std::stringstream &ss, int level);
+
+        const bool has_children();
 
         trie_node &operator=(const trie_node &other);
 
@@ -56,11 +67,14 @@ private:
         trie_node &operator=(trie_node &&other) noexcept;
 
         friend std::ostream &operator<<(std::ostream &out, const trie_node &other);
+
     };
 
     trie_node *_root;
-    size_t _num_ngrams;
     size_t _num_nodes;
+    size_t _num_ngrams;
+
+    bool r_delete_helper(const mstd::vector<std::string> &ngram, trie_node *current, int length, int level, int *found);
 public:
     trie();
     ~trie();
@@ -69,13 +83,19 @@ public:
 
     void search(const mstd::vector<std::string> &ngram, mstd::queue<std::string> *results);
 
-    void print_tree();
+    bool delete_ngram(const mstd::vector<std::string> &ngram);
+
+    bool r_delete_ngram(const mstd::vector<std::string> &ngram);
 
     size_t get_num_nodes();
 
     size_t get_num_ngrams();
 
     friend std::ostream &operator<<(std::ostream &out, const trie_node &other);
+
+    void print_tree();
+
+    std::string to_string();
 };
 
 #endif // TRIE
