09cdf85d53f31e3680c07a013536c9253e571304
Giannis Argyros <j_silver1994@hotmail.com>
2018-01-06 03:11:41 +0200

    More specific

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]; then
     rm -rf build/*
 elif [ "$1" == "-g" ]; then
-    rm -rf build/CMake* build/cmake* build/Makefile
+    rm -rf build/CMakeFiles build/cmake_install* build/Makefile build/CMakeCache.txt
 else
     echo "Failed to clean"
 fi
